7f025947e5841fe18ad4fb459e4c9cf6fd3b25aa
developersworkspace@gmail.com <developersworkspace@gmail.com>
2017-02-11 17:54:56 +0200

    Adds models

---
+++ web/src/app/models/supplement-feedstuff.ts
@@ -0,0 +1,12 @@
+export class SupplementFeedstuff {
+    
+    id: string;
+    text: string;
+    weight: number;
+
+    constructor(id: string, text: string, weight: number) {
+        this.id = id;
+        this.text = text;
+        this.weight = weight;
+    }
+}
