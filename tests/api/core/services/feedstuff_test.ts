d3d8b5f6b1b7e6484ed7560585d5161b4056c65e
Barend Erasmus <developersworkspace@gmail.com>
2017-02-16 08:14:37 +0200

    Full coverage on feedstuff service

--- tests/api/core/services/feedstuff_test.ts
+++ tests/api/core/services/feedstuff_test.ts
@@ -16,17 +16,24 @@ import { SupplementElement as DomainSupplementElement } from './../../../../api/
 describe('FeedstuffService', () => {
 
     let feedstuffService: FeedstuffService = null;
-    beforeEach(function (done: Function) {
+
+    let existingFeedstuffId = '36995DEF-9B6B-49CD-9AA6-0165478BA4CD';
+    let nonExistingFeedstuffId = '078567E3-A67E-4737-B273-4AE381FDBACD';
+    let existingFormulaIdWithSuggestedValue = 'CB0360F3-4617-4922-B20D-C3F223BBBCEB';
+    let existingFeedstuffIdWithSuggestedValue = 'B3EDBFD3-CB3C-4427-A6FB-B20EBF4FC831';
+    let nonExistingFormulaId = '3DE8F48A-CC7A-4217-B317-979866B42BB6';
+    let existingElementIdWithSupplementFeedstuff = '93E1BC7C-6D08-424E-9DB6-E4BA09E339C5';
+
+    beforeEach(() => {
 
         feedstuffService = new FeedstuffService({
-                db: {
-                    server: '127.0.0.1',
-                    user: 'worldofrations_user',
-                    password: 'worldofrations_password',
-                    database: 'worldofrations'
-                }
-            })
-        done();
+            db: {
+                server: '127.0.0.1',
+                user: 'worldofrations_user',
+                password: 'worldofrations_password',
+                database: 'worldofrations'
+            }
+        });
     });
 
     describe('listFeedstuffs', () => {
@@ -38,7 +45,6 @@ describe('FeedstuffService', () => {
         });
     });
 
-
     describe('listExampleFeedstuffs', () => {
         it('should return list of feedstuffs', () => {
             return feedstuffService.listExampleFeedstuffs().then((result: DomainFeedstuff[]) => {
@@ -49,18 +55,35 @@ describe('FeedstuffService', () => {
     });
 
     describe('getSuggestedValues', () => {
-
-        it('should return list of suggested values', () => {
-            return feedstuffService.getSuggestedValues('CB0360F3-4617-4922-B20D-C3F223BBBCEB', '0CA0953B-C75B-48A1-A31D-89FACC248E90').then((result: DomainSuggestedValue) => {
+        it('should return suggested value given existing formula id and existing feedstuff id', () => {
+            return feedstuffService.getSuggestedValues(existingFormulaIdWithSuggestedValue, existingFeedstuffIdWithSuggestedValue).then((result: DomainSuggestedValue) => {
                 expect(result).to.be.not.null;
             });
         });
-
-        it('should return empty list given feedstuff id with no suggested values', () => {
-            return feedstuffService.getSuggestedValues('CB0360F3-4617-4922-B20D-C3F223BBBCEB', '6B5F4F25-6661-4D4B-ABC7-D44AEFCDE955').then((result: any) => {
+        it('should return null given non-existing formula id and existing feedstuff id', () => {
+            return feedstuffService.getSuggestedValues(nonExistingFormulaId, nonExistingFeedstuffId).then((result: DomainSuggestedValue) => {
                 expect(result).to.be.null;
             });
         });
-
     });
+
+    describe('loadElementsForFeedstuffs', () => {
+        it('hould return list of feedstuffs with elements populated', () => {
+            return feedstuffService.loadElementsForFeedstuffs([new DomainFeedstuff(existingFeedstuffId, null, 0, 1000, 2000)]).then((result: DomainFeedstuff[]) => {
+                expect(result).to.be.not.null;
+                expect(result[0].elements.length).to.be.greaterThan(0);
+            });
+        });
+    });
+
+
+    describe('loadNamesForFeedstuffs', () => {
+        it('hould return list of feedstuffs with names populated', () => {
+            return feedstuffService.loadNamesForFeedstuffs([new DomainFeedstuff(existingFeedstuffId, null, 0, 1000, 2000)]).then((result: DomainFeedstuff[]) => {
+                expect(result).to.be.not.null;
+                expect(result[0].name).to.be.not.null;
+            });
+        });
+    });
+
 });
