ecaa6c0a76746a099ef46cfc009df6f14b553364
Brendan.Huysamen <developersworkspace@gmail.com>
2017-02-28 10:07:37 +0200

    refactors mongo repository

--- api/src/core/repositories/mongo/formulation.ts
+++ api/src/core/repositories/mongo/formulation.ts
@@ -11,46 +11,25 @@ export class FormulationRepository {
 
     }
 
-    public saveFormulation(formulation: DomainFormulation): Promise<Boolean> {
-        return new Promise((resolve: Function, reject: Function) => {
-            winston.profile('FormulationRepository.saveFormulation');
-            let mongoClient = new mongodb.MongoClient();
-            mongoClient.connect('mongodb://' + this.config.server + ':27017/' + this.config.database, (err: Error, db: mongodb.Db) => {
-                if (err) {
-                    reject(err);
-                    winston.profile('FormulationRepository.saveFormulation');
-                } else {
-                    var collection = db.collection('formulations');
-                    formulation.clean();
-                    collection.insertOne(formulation, (err: Error, result: any) => {
-                        db.close();
-                        resolve(true);
-                        winston.profile('FormulationRepository.saveFormulation');
-                    });
-                }
+    public saveFormulation(formulation: DomainFormulation): Promise<any> {
+        let mongoClient = new mongodb.MongoClient();
+        return mongoClient.connect('mongodb://' + this.config.server + ':27017/' + this.config.database).then((db: mongodb.Db) => {
+            var collection = db.collection('formulations');
+            formulation.clean();
+            return collection.insertOne(formulation).then((result: any) => {
+                db.close();
+                return true;
             });
         });
     }
 
-    public getFormulationById(formulationId: string): Promise<DomainFormulation> {
-        return new Promise((resolve: Function, reject: Function) => {
-            winston.profile('FormulationRepository.getFormulationById');
-            let mongoClient = new mongodb.MongoClient();
-            mongoClient.connect('mongodb://' + this.config.server + ':27017/' + this.config.database, (err: Error, db: mongodb.Db) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    var collection = db.collection('formulations');
-                    collection.findOne({ id: formulationId }, (err: Error, formulation: DomainFormulation) => {
-                        if (err) {
-                            reject(err);
-                            winston.profile('FormulationRepository.getFormulationById');
-                        } else {
-                            resolve(formulation);
-                            winston.profile('FormulationRepository.getFormulationById');
-                        }
-                    });
-                }
+    public getFormulationById(formulationId: string): Promise<any> {
+        let mongoClient = new mongodb.MongoClient();
+        return mongoClient.connect('mongodb://' + this.config.server + ':27017/' + this.config.database).then((db: mongodb.Db) => {
+            var collection = db.collection('formulations');
+            return collection.findOne({ id: formulationId }).then((formulation: DomainFormulation) => {
+                db.close();
+                return formulation;
             });
         });
     }
