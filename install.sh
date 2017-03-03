50bc8d048e3f8ce5bc69e699dc2e0f6ddca31271
developersworkspace@gmail.com <developersworkspace@gmail.com>
2017-03-03 18:26:00 +0200

    update

--- install.sh
+++ install.sh
@@ -45,6 +45,20 @@ cd ./../
 # Build and run docker compose as deamon
 docker-compose up -d
 
+# -- INSTALL SSL CERT --
+
+# Update machine package indexes
+sudo apt-get update
+
+# Open 443 port
+sudo ufw allow 443/tcp
+
+# Install Let's Encrypt cli
+sudo apt-get install -y letsencrypt
+
+# Obtain SSL CERT
+sudo letsencrypt certonly --agree-tos --standalone --email developersworkspace@gmail.com -d worldofrations.com
+
 # -- INSTALL NGINX --
 
 # Update machine package indexes
