0f80b2ad16ee68021a9b9470e9ab556fb87797b3
Brendan.Huysamen <developersworkspace@gmail.com>
2017-03-20 10:54:22 +0200

    adds formulation sql scripts

---
+++ mysql/scripts/formulationFeedstuffs.sql
@@ -0,0 +1,7 @@
+CREATE TABLE worldofrations.formulations(
+    `formulationId` CHAR(36)        NOT NULL,
+    `feedstuffId`   CHAR(36)        NOT NULL,
+    `minimum`       NUMERIC(10,3)   NULL,
+    `maximum`       NUMERIC(10,3)   NOT NULL,
+    `cost`          NUMERIC(10,3)   NOT NULL
+);
---
+++ mysql/scripts/formulationResultFeedstuffs.sql
@@ -0,0 +1,7 @@
+CREATE TABLE worldofrations.formulations(
+    `formulationId` CHAR(36)        NOT NULL,
+    `feedstuffId`   CHAR(36)        NOT NULL,
+    `weight`        NUMERIC(10,3)   NOT NULL,
+    `status`        CHAR(64)        NOT NULL,
+    `cost`          NUMERIC(10,3)   NOT NULL
+);
